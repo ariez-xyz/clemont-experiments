65b7be9bd8cb50a57432a1b94c01e6275bfb3d9e
ariez <dpape39@icloud.com>
2024-12-31 13:57:12 +0100

    actually add the scripts

--- experiments/slurm_robusttrees_ijcnn_eps_sweep_submit.sh
+++ experiments/slurm_robusttrees_ijcnn_eps_sweep_submit.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 # Main Configuration
-robust_input="../data/RobustTrees/predictions/ijcnn/test_pred.csv"
-unrob_input="../data/RobustTrees/predictions/ijcnn.unrob/test_pred.csv"
-results_base="../results/robusttrees_ijcnn_eps_sweep_slurm"
-work_script="slurm_robusttrees_ijcnn_eps_sweep_work.sh"
-array="1,3"
+export robust_input="../data/RobustTrees/predictions/ijcnn/test_pred.csv"
+export unrob_input="../data/RobustTrees/predictions/ijcnn.unrob/test_pred.csv"
+export results_base="../results/slurm_robusttrees_ijcnn_eps_sweep"
+export work_script="slurm_robusttrees_ijcnn_eps_sweep_work.sh"
+export array="1-100"
 
 # Housekeeping
-results_dir="$results_base/results"
-logs_dir="$results_base/logs"
+export results_dir="$results_base/results"
+export logs_dir="$results_base/logs"
 unset SLURM_EXPORT_ENV
 mkdir -p "$results_base"
 mkdir -p "$results_dir"
 mkdir -p "$logs_dir"
+pushd ..
+source activate.sh
+popd
 
 # Submit to queue
-sbatch --array=$array --job-name=$work_script --output=$logs_dir/%A-%a.log -c 1 --time=0:01:00 --mem=1G --no-requeue --export=NONE "srun $work_script"
+sbatch --array=$array --job-name=$work_script --output=$logs_dir/%A-%a.log -c 1 --time=0:00:30 --mem=1G --no-requeue --export=ALL $work_script
 
--- experiments/slurm_robusttrees_ijcnn_eps_sweep_work.sh
+++ experiments/slurm_robusttrees_ijcnn_eps_sweep_work.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-pushd ..
-source activate.sh
-popd
 
-# Calculate epsilon value based on array task ID
-eps=$(echo "scale=3; $SLURM_ARRAY_TASK_ID * 0.001" | bc)
-eps_str=$(printf "%.3f" $eps)
-echo "$eps_str"
+# calculate epsilon based on array job ID
+eps=$(python3 -c "import sys; print('{:.3f}'.format($SLURM_ARRAY_TASK_ID * 0.001))")
 
-python run_on_csv.py "$robust_input" --max_n 10000 --eps "$eps" --out_path "$results_dir/rob-$eps_str.json"
-python run_on_csv.py "$unrob_input" --max_n 10000 --eps "$eps" --out_path "$results_dir/unrob-$eps_str.json"
+srun python run_on_csv.py "$robust_input" --max_n 10000 --eps "$eps" --out_path "$results_dir/rob-$eps.json" \
+  && python run_on_csv.py "$unrob_input"  --max_n 10000 --eps "$eps" --out_path "$results_dir/unrob-$eps.json"
 
