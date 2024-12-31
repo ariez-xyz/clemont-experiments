d6a1f73997fc65b7fff3db337aa6c42978f59eed
ariez <dpape39@icloud.com>
2024-12-31 14:37:51 +0100

    experiment: robusttrees ijcnn eps sweep full train set

---
+++ experiments/slurm_robusttrees_ijcnn_eps_sweep_train_set_submit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Main Configuration
+export robust_input="../data/RobustTrees/predictions/ijcnn/train_pred.csv"
+export unrob_input="../data/RobustTrees/predictions/ijcnn.unrob/train_pred.csv"
+export results_base="../results/slurm_robusttrees_ijcnn_eps_sweep_train_set"
+export work_script="slurm_robusttrees_ijcnn_eps_sweep_work.sh"
+export array="1-100"
+
+# Housekeeping
+export results_dir="$results_base/results"
+export logs_dir="$results_base/logs"
+unset SLURM_EXPORT_ENV
+mkdir -p "$results_base"
+mkdir -p "$results_dir"
+mkdir -p "$logs_dir"
+pushd ..
+source activate.sh
+popd
+
+# Submit to queue
+sbatch --array=$array --job-name=$work_script --output=$logs_dir/%A-%a.log -c 1 --time=0:05:00 --mem=1G --no-requeue --export=ALL $work_script
+
