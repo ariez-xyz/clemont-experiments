4c77937e2ac53123769f2595812f23f218b596cd
ariez <dpape39@icloud.com>
2025-01-21 11:54:09 +0100

    CHECKPOINT: upgraded run_on_csv.py script. 33 positives on lcifr adult dl0 vs. 7 for dl10

--- experiments/run_on_csv.py
+++ experiments/run_on_csv.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import argparse
 import json
+import time
 from datetime import datetime
 
 from aimon.backends.bdd import BDD
@@ -11,37 +12,60 @@ from aimon.runner import Runner
 np.set_printoptions(suppress=True)
 
 def log(s):
-    print(s, flush=True)
+    timestamp = datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    print(f"[{timestamp}] {s}", flush=True)
 
-def pprint_pair(df, i, j, eps):
-    print(f'\nrow {i}\trow {j}\tdiff\t<{eps}?')
+def fatal(s):
+    log("fatal: " + s)
+    exit(1)
+
+def pretty_print(df, i, j, eps, header=True, diff_only=False, marker=" "):
+    if header:
+        print(f'\n {"column".rjust(30)}\trow {i}\trow {j}\tdelta')
+
+    differing_attrs = []
+    rest = []
     for col in range(len(df.columns)):
         val_i = df.iloc[i, col]
         val_j = df.iloc[j, col]
         diff = abs(val_i - val_j)
         is_close = diff < eps
-        print(f"{val_i:.4f}\t{val_j:.4f}\t{diff:.4f}\t{is_close}")
+        if is_close:
+            line = f"{marker}{df.columns[col][:30].rjust(30)}\t{val_i:.6f}\t{val_j:.6f}\t{diff:.6f}"
+            rest.append(line)
+        else:
+            line = f"\033[91m{marker}{df.columns[col][:30].rjust(30)}\t{val_i:.6f}\t{val_j:.6f}\t{diff:.6f}\033[0m"
+            differing_attrs.append(line)
 
-if __name__ == "__main__":
+    for line in differing_attrs:
+        print(line)
+    if not diff_only:
+        for line in rest:
+            print(line)
+
+def make_argparser():
     parser = argparse.ArgumentParser(description='run monitor on csv format predictions')
     parser.add_argument('csvpath', type=str, help='Path to the CSV file')
     parser.add_argument('--eps', type=float, help='epsilon')
-    parser.add_argument('--n_bins', type=int, help='Number of bins')
-    parser.add_argument('--max_n', type=int, help='Cap the number of samples to process', default=-1)
-    parser.add_argument('--out_path', type=str, help='Path to save output JSON')
-    parser.add_argument('--full_output', action='store_true', help='Flag to print full output')
-    parser.add_argument('--randomize_order', action='store_true', help='Randomize CSV order')
+    parser.add_argument('--n_bins', '--n-bins', type=int, help='Number of bins')
+    parser.add_argument('--n_examples', '--n-examples', type=int, help='Cap the number of samples to process', default=-1)
+    parser.add_argument('--out_path', '--out-path', type=str, help='Path to save output JSON')
+    parser.add_argument('--full_output', '--full-output', action='store_true', help='verbose output (timings, concrete counterexample pairs)')
+    parser.add_argument('--randomize_order', '--randomize-order', action='store_true', help='Randomize CSV order')
+    parser.add_argument('--backend', type=str, default='bf', choices=['bf', 'bdd'], help='which implementation to use as backend')
+    parser.add_argument('--blind_cols', '--blind-cols', type=str, help='comma-separated list of sensitive columns, e.g. "race,sex". allows wildcards like "race=*"')
+    parser.add_argument('--pred', type=str, default='pred', help='name of the column holding model predictions')
+    return parser
     
-    args = parser.parse_args()
+if __name__ == "__main__":
+    args = make_argparser().parse_args()
     csvpath = args.csvpath
 
     # user must provide exactly 1 of eps or n_bins
     if args.eps and args.n_bins:
-        print("need one of --eps or --n_bins, not both")
-        exit(1)
+        fatal("need one of --eps or --n_bins, not both")
     elif not args.n_bins and not args.eps:
-        print("need one of --eps or --n_bins")
-        exit(1)
+        fatal("need one of --eps or --n_bins")
 
     # infer the respective missing arg
     if args.n_bins:
@@ -51,29 +75,57 @@ if __name__ == "__main__":
 
     log(f"loading {csvpath}...")
     df = pd.read_csv(csvpath) 
-    log(f"loaded {df.shape} df")
+
+    log(f"loaded data of shape {df.shape}.")
+
+    if args.blind_cols:
+        cols_to_drop = []
+        for expr in args.blind_cols.split(","):
+            if expr[-1] == "*": # wildcard
+                for col in df.columns:
+                    if col.startswith(expr[:-1]):
+                        cols_to_drop.append(col)
+            else: # literal
+                cols_to_drop.append(expr)
+        # Separate into dropped and remaining columns
+        blind_df = df[cols_to_drop].copy()
+        df.drop(columns=cols_to_drop, inplace=True)
+        log(f"dropped sensitive columns {cols_to_drop} (new shape is {df.shape})")
+
     if args.randomize_order:
         df = df.sample(frac=1).reset_index(drop=True)  # Randomize the dataframe rows
+        log(f"randomized order")
+
     low_cardinality_cols = [col for col in df.columns if df[col].nunique() < args.n_bins]
-    log(f"assuming {low_cardinality_cols} for categorical attributes (must be exact match)")
+    log(f"low-cardinality columns: {low_cardinality_cols}. assuming categorical (i.e, must be exact match)")
+
     num_columns = df.shape[1]
 
-    #bdd = BDD(
-    #    data_sample=df,
-    #    n_bins=args.n_bins,
-    #    decision_col='pred',
-    #    categorical_cols=low_cardinality_cols,
-    #    collect_cex=True
-    #)
+    if args.backend == 'bdd':
+        log(f"initializing BDD backend...")
+        backend = BDD(
+            data_sample=df,
+            n_bins=args.n_bins,
+            decision_col=args.pred,
+            categorical_cols=low_cardinality_cols,
+            collect_cex=True
+        )
+    elif args.backend == 'bf':
+        log(f"initializing brute force backend...")
+        backend = BruteForce(df, args.pred, args.eps)
 
-    bf = BruteForce(df, 'pred', args.eps)
-
-    #runner = Runner(bdd)
-    runner = Runner(bf)
+    runner = Runner(backend)
 
     log(f"starting...")
+    monitor_positives = sorted(runner.run(df, args.n_examples))
 
-    monitor_positives = sorted(runner.run(df, args.max_n))
+    if args.full_output:
+        for pair in monitor_positives:
+            if blind_df is not None:
+                pretty_print(df, pair[0], pair[1], args.eps)
+                pretty_print(blind_df, pair[0], pair[1], args.eps, marker="*", header=False)
+            else:
+                pretty_print(df, pair[0], pair[1], args.eps)
 
     log(f'found {runner.n_true_positives} unfair pairs')
 
@@ -96,8 +148,4 @@ if __name__ == "__main__":
         with open(args.out_path, 'w') as f:
             json.dump(out, f, indent=2)
 
-    elif args.full_output:
-        print(monitor_positives)
-        for pair in monitor_positives:
-            pprint_pair(df, pair[0], pair[1], args.eps)
 
