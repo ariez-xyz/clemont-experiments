9977dfaf68f1f03519f869d73d98b2a39aa05c4d
ariez <dpape39@icloud.com>
2025-02-05 07:47:07 +0100

    add initial script to collect stats overview for an experiment

--- results/get_stats.py
+++ results/get_stats.py
@@ -10,20 +10,45 @@ directory = Path(sys.argv[1])
 for json_file in directory.glob('**/*.json'):
     with open(json_file) as f:
         data = json.load(f)
+        print(json_file, file=sys.stderr)
+        if type(data['args']) == list:
+            data['args'] = data['args'][0]
+        if type(data['peak_mem']) == list:
+            data['peak_mem'] = data['peak_mem'][0]
         
         # Extract the basename from the csvpath
-        csvpath = data['args'][0]['csvpath'][0]
+        csvpath = data['args']['csvpath'][0]
         basename = os.path.basename(csvpath)
-        
+
+        if 'parallelize' in data['args'].keys():
+            backend = f"{data['args']['backend']}@{data['args']['parallelize']}t"
+        else:
+            backend = data['args']['backend']
+
+        i = 0
+        worker_mem_deltas = []
+        while f'worker_{i}' in data.keys():
+            worker_mem_deltas.append(data[f'worker_{i}']['peak_mem'] - data[f'worker_{i}']['mem'][0]/1024)
+            i += 1
+        if worker_mem_deltas:
+            mem_delta = sum(worker_mem_deltas)/len(worker_mem_deltas)
+        else:
+            mem_delta = data['peak_mem'] - data['mem'][0]/1024
+
         # Create tuple with required fields
         result_tuple = (
             basename,
-            data['args'][0]['eps'],
+            data['args']['eps'],
             data['avg_time'],
-            data['peak_mem'][0]
+            backend,
+            #round(mem_delta),
+            data['peak_mem'],
+            len(data['positives']),
         )
         results.append(result_tuple)
 
 # Sort and print results
+#print("filename, eps, avg time, backend, memory delta between first and last iter (averaged per-worker if parallelized), n_positives")
+#print("filename, eps, avg time, backend, peak memory recorded by main process, n_positives")
 for result in sorted(results):
     print(result)
