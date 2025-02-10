e698c736640709c545e5b75ef4b29c3321bb3297
ariez <dpape39@icloud.com>
2025-02-10 09:58:53 +0100

    add lower dims to cut_dims.py

--- data/RobustBench/cut_dims.py
+++ data/RobustBench/cut_dims.py
@@ -4,7 +4,7 @@ i1="predictions/imagenet-Standard_R50.csv"
 i2="predictions/imagenet-Standard_R50-adv.csv"
 
 pred="pred"
-samplecols=map(int, "8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32767 65536 131072".split())
+samplecols=map(int, "2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32767 65536 131072".split())
 
 # Shamelessly ripped from run_on_csv.py
 dfs = []
