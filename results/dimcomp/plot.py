156df4a04617bb2f05754f8e724bcfedbfcb95d0
ariez <dpape39@icloud.com>
2025-02-10 11:10:38 +0100

    add dashed line to dimcomp?

--- results/dimcomp/plot.py
+++ results/dimcomp/plot.py
@@ -46,6 +46,14 @@ for method in methods:
         "bf": "Brute force (Linf)",
     }
     plt.plot(dimensions, avg_times, marker='o', label=method_displaynames[method])
+    if method == 'bdd':
+        last_y_bdd = avg_times[np.where(method == "bdd")[0]][-1]
+        plt.plot([2**6, 2**7], [last_y_bdd, 10], 
+                 color='blue', linestyle='--', alpha=0.5)
+    if method == 'snn':
+        last_y_snn = avg_times[np.where(method == "l2-snn")[0]][-1]
+        plt.plot([2**15, 2**16], [last_y_snn, 10], 
+                 color='orange', linestyle='--', alpha=0.5)
 
 plt.xscale('log', base=2)
 plt.yscale('log')
@@ -53,6 +61,9 @@ plt.xlabel('Number of Dimensions')
 plt.ylabel('Average Processing Time (seconds)')
 #plt.title('Processing Time vs Dimensions by Method')
 plt.grid(True, which="both", ls="-", alpha=0.5)
+
+
+
 plt.legend()
 plt.tight_layout()
 plt.savefig('dimensions.png', dpi=300)
