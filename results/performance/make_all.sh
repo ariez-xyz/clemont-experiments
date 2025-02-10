933e88dc57be6eb2f41c4007947b4a8114d7d8ef
ariez <dpape39@icloud.com>
2025-02-10 10:49:05 +0100

    cleaner names

--- results/performance/make_all.sh
+++ results/performance/make_all.sh
@@ -1,9 +1,9 @@
-for d in 11: 23:; do 
+for d in 11 23; do 
 	for eps in 0.05 0.025 0.01; do 
 		for run in 2; do 
 			python plot.py --eps $eps \
 				--run $run \
-				--sample $d \
+				--sample $d: \
 				--truncate 10000000 \
 				--outfile $d-$eps.png; 
 		done; 
