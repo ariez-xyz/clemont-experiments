26a200c124cb42e2dd493d228e224c441958adb4
ariez <dpape39@icloud.com>
2025-02-10 09:53:09 +0100

    cleanup repo

--- results/semantic/makeall.sh
+++ results/semantic/makeall.sh
@@ -1,5 +1,5 @@
 python plot.py --rate cifar_100c/ --out cifar100c.png
 python plot.py --rate cifar_10c/ --out cifar10c.png
 python plot.py --rate cifars/ --out cifars.png
-python plot.py --rate tmp_imagenet/ --out imagenet.png
+python plot.py --rate imagenet/ --out imagenet.png
 
