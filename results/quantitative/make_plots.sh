4fa6082f1a6bc826ef201e3d01bca816b1215d14
ariez <dpape39@icloud.com>
2025-10-17 16:54:20 +0200

    update naming

--- results/quantitative/make_plots.sh
+++ results/quantitative/make_plots.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Plotting time by ratio percentile..."
-python plot_time_by_ratio_percentile.py certifair/baseline_no_maxk/quant_run_20251014T190907.json certifair/fair_no_maxk/quant_run_20251014T155455.json --fill-percentiles 20,80 --bins 20 --no-title --labels 'Naive model,Fair model'
+python plot_time_by_ratio_percentile.py certifair/fair_no_maxk/quant_run_20251014T155455.json certifair/baseline_no_maxk/quant_run_20251014T190907.json --fill-percentiles 20,80 --bins 20 --no-title --labels 'Fair model,Baseline model'
 
 echo "Plotting progressions percentiles..."
 python plot_progressions_percentiles.py certifair/fair_kgrow_1p05/
 
 echo "Plotting max ratio combined (certifair)..."
-python plot_max_ratio_combined.py certifair/fair_no_maxk/quant_run_20251015T205531.json certifair/baseline_no_maxk/quant_run_20251015T192207.json --output certifair/ --labels 'Fair model,Naive model' --bins 40 --no-title
+python plot_max_ratio_combined.py certifair/fair_no_maxk/quant_run_20251015T205531.json certifair/baseline_no_maxk/quant_run_20251015T192207.json --output certifair/ --labels 'Fair model,Baseline model' --bins 40 --no-title
 
 echo "Plotting max ratio combined (robustbench baseline)..."
 python plot_max_ratio_combined.py robustbench_cifar10/baseline/quant_run_20251015T143006.json --split 9999 --output robustbench_cifar10/hist_naive.png --bins 40 --labels 'Real images,Adversarial examples' --no-title
@@ -23,7 +23,7 @@ echo "Plotting max ratio combined (eps 0.1)..."
 python plot_max_ratio_combined.py certifair/fair_eps_0p100_maxk_none/quant_run_20251015T204847.json --split-epsilon-flagged --no-title --output certifair/hist_flag_vs_nonflag_eps_0.1.png --bins 40
 
 echo "Plotting quant/qual MSE..."
- python plot_epsilon_flag_alignment.py certifair --series "fair=fair*none" --series "base=base*none" --output certifair/fair_vs_base_alignment.png
+ python plot_epsilon_flag_alignment.py certifair --series "Fair model=fair*none" --series "Baseline model=base*none" --output certifair/fair_vs_base_alignment.png
 
 echo "Plotting time per sample..."
 python plot_time_per_sample.py robusttrees_higgs/results/ \
